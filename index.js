2e733eb7dc23b94fa96464d1cab0e2402c8b941e
arnold <arnoldkmaina@gmail.com>
2024-03-21 21:26:57 +0300

    arrow function lab

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide () {
+    return 2000 / 100
+}
+let square = (a, b) => {
+    const result = a * a;
+    return result;
+};
+
+let add = (a, b) => {
+    const result = a + b;
+    return result;
+};
